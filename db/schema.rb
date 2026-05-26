f2392a40c1794a54eefa36437dd9d40202f90290
Shun Fujita <sfujit123456@gmail.com>
2026-05-25 22:31:35 -0500

    Add priority flag to todos for toggle state

--- db/schema.rb
+++ db/schema.rb
@@ -16,5 +16,6 @@ ActiveRecord::Schema[8.0].define(version: 2026_05_19_180933) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "due_date"
+    t.boolean "priority", default: false, null: false
   end
 end
